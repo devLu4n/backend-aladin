b7103d950a0b10fc292c516ae9c09dbecd510fbb
Luan Vilela <luanvilela11@gmail.com>
2026-07-10 19:58:15 -0300

    feat: adicionar fluxo completo de candidaturas

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,6 +4,7 @@ const authRoutes = require("./auth.routes");
 const empresasRoutes = require("./empresas.routes");
 const vagasRoutes = require("./vagas.routes");
 const candidatosRoutes = require("./candidatos.routes");
+const candidaturasRoutes = require("./candidaturas.routes");
 
 const router = Router();
 
@@ -15,5 +16,6 @@ router.use("/auth", authRoutes);
 router.use("/empresas", empresasRoutes);
 router.use("/vagas", vagasRoutes);
 router.use("/candidatos", candidatosRoutes);
+router.use("/candidaturas", candidaturasRoutes);
 
 module.exports = router;
